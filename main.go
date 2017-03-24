3a0c664d17bac586408b6defa4c3378f42245eb2
Bjørn Åge Tungesvik <bjorn.tungesvik@gmail.com>
2017-03-24 15:00:52 +0100

    asdf

--- main.go
+++ main.go
@@ -13,19 +13,16 @@ func main() {
 	//Gather input variables in struct
 	params := parameters.Get()
 
-
-
-
-  filename := fmt.Sprintf("%s.%s", "app", params.DistributionType)
+	filename := fmt.Sprintf("%s.%s", "app", params.DistributionType)
 	//Find a cleaner solution
 	program.DownloadFromUrl(params, filename)
 	if strings.Contains(filename, ".zip") {
 		program.Unzip(filename)
+		docker.BuildDockerImage(params, "app")
 	} else if strings.Contains(filename, ".jar") {
 		params.Command = fmt.Sprintf("java -jar %s", filename)
+		docker.BuildDockerImage(params, filename)
 	}
-
-	docker.BuildDockerImage(params, filename)
 
 	docker.TagDockerImages(params)
 
